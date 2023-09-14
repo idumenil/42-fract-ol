d708dbb81eec2801ca78d5b747a929858824adb3
Irène Duménil <126900447+idumenil@users.noreply.github.com>
2023-09-14 09:38:36 +0000

    Update render.c

--- render.c
+++ render.c
@@ -1,75 +1,67 @@
 #include "fractol.h"
 #include "minilibx-linux/mlx.h"
+#include <X11/X.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-static void	my_pixel_put(int x, int y, t_img *img, int color)
+
+int	close_handler(t_fractal *fractal)
 {
-	int	offset;
-
-	offset = (y * img->line_len) + (x * (img->bpp / 8));
-	*(unsigned int *)(img->pixels_ptr + offset) = color;
+	mlx_destroy_image(fractal->mlx_connection,
+					fractal->img.img_ptr);
+	mlx_destroy_window(fractal->mlx_connection,
+						fractal->mlx_window);
+	mlx_destroy_display(fractal->mlx_connection);
+	free(fractal->mlx_connection);
+	exit(EXIT_SUCCESS);
 }
-	
-static void	mandel_vs_julia(t_complex *z, t_complex *c, t_fractal *fractal)
-{	
+
+int	key_handler(int keysym, t_fractal *fractal)
+{
+	if (keysym == XK_Escape)
+		close_handler(fractal);
+	if (keysym == XK_Left)
+		fractal->shift_x += (0.5 * fractal->zoom);	
+	else if (keysym == XK_Right)
+		fractal->shift_x -= (0.5 * fractal->zoom);	
+	else if (keysym == XK_Up)
+		fractal->shift_y -= (0.5 * fractal->zoom);	
+	else if (keysym == XK_Down)
+		fractal->shift_y += (0.5 * fractal->zoom);	
+	else if (keysym == XK_plus)
+		fractal->iterations_defintion += 10;
+	else if (keysym == XK_minus)	
+		fractal->iterations_defintion -= 10;
+
+	// refresh the image
+	fractal_render(fractal);
+	return 0;
+}
+
+int	mouse_handler(int button, int x, int y, t_fractal *fractal)
+{
+	//Zoom in
+	if (button == Button5)
+	{
+		fractal->zoom *= 0.95;
+	}
+	//Zoom out
+	else if (button == Button4)
+	{
+		fractal->zoom *= 1.05;
+	}
+	// refresh
+	fractal_render(fractal);
+	return 0;
+}
+
+int	julia_track(int x, int y, t_fractal *fractal)
+{
 	if (!ft_strncmp(fractal->name, "julia", 5))
 	{
-		c->x = fractal->julia_x;
-		c->y = fractal->julia_y;
+		fractal->julia_x = (map(x, -2, +2, 0, WIDTH) * fractal->zoom) + fractal->shift_x;
+		fractal->julia_y = (map(y, +2, -2, 0, HEIGHT) * fractal->zoom) + fractal->shift_y;
+		fractal_render(fractal);
 	}
-	else
-	{
-		c->x = z->x;
-		c->y = z->y;
-	}
-}
-
-static void	handle_pixel(int x, int y, t_fractal *fractal)
-{
-	t_complex	z;
-	t_complex	c;
-	int			i;
-	int			color;
-
-	i = 0;
-	z.x = (map(x, -2, +2, 0, WIDTH) * fractal->zoom) + fractal->shift_x;
-	z.y = (map(y, +2, -2, 0, HEIGHT) * fractal->zoom) + fractal->shift_y;
-
-
-	mandel_vs_julia(&z, &c, fractal);
-
-	while (i < fractal->iterations_defintion)
-	{
-		z = sum_complex(square_complex(z), c);
-		
-		if ((z.x * z.x) + (z.y * z.y) > fractal->escape_value)
-		{
-			color = map(i, BLACK, WHITE, 0, fractal->iterations_defintion);
-			my_pixel_put(x, y, &fractal->img, color);
-			return ;
-		}
-		++i;	
-	}
-	my_pixel_put(x, y, &fractal->img, WHITE);
-}
-
-
-void	fractal_render(t_fractal *fractal)
-{
-	int	x;
-	int	y;
-
-	y = -1;
-	while (++y < HEIGHT)
-	{
-		x = -1;
-		while (++x < WIDTH)
-		{
-			handle_pixel(x, y, fractal);
-		}
-	}
-	mlx_put_image_to_window(fractal->mlx_connection,
-							fractal->mlx_window,
-							fractal->img.img_ptr,
-							0, 0);	
-
+	return 0;
 }
