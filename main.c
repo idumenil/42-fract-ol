1d603f66d1e57a16fc18e3d105300705a3dadf5f
Irène Duménil <126900447+idumenil@users.noreply.github.com>
2023-09-14 09:34:07 +0000

    Update main.c

--- main.c
+++ main.c
@@ -11,23 +11,29 @@
 /* ************************************************************************** */
 
 #include "fractol.h"
+#include "minilibx-linux/mlx.h"
 
-
-int main(int argc, char **argv)
+int	main(int argc, char **av)
 {
 	t_fractal	fractal;
 
-	if (argc == 2 && !ft_strncmp(argv[1], "mandelbrot", 10)
-	|| argc == 4 && !ft_strncmp(argv[1], "julia", 5))
+
+	if (argc == 2 && !ft_strncmp(av[1], "mandelbrot", 10)
+		|| argc == 4 && !ft_strncmp(av[1], "julia", 5))
 	{
-		fractal.name = argv[1];
-		write(1, "ok\n", 3);		
+		fractal.name = av[1];
+		if (!ft_strncmp(fractal.name, "julia", 5))
+		{
+			fractal.julia_x = atodbl(av[2]);
+			fractal.julia_y = atodbl(av[3]);
+		}
 		fractal_init(&fractal);
-//		fractal_render(&fractal);
+		fractal_render(&fractal);
 		mlx_loop(fractal.mlx_connection);
 	}
 	else
 	{
-		write(1, "nok\n", 4);
+		putstr_fd(ERROR_MESSAGE, STDERR_FILENO);
+		exit(EXIT_FAILURE);
 	}
 }
