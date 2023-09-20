cb0ea0b7af847467e622dbb77db40a8493590439
Irène Duménil <126900447+idumenil@users.noreply.github.com>
2023-09-20 14:49:25 +0200

    Update fractol.h

--- fractol.h
+++ fractol.h
@@ -6,7 +6,7 @@
 /*   By: idumenil <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/07 15:52:41 by idumenil          #+#    #+#             */
-/*   Updated: 2023/09/07 15:53:52 by idumenil         ###   ########.fr       */
+/*   Updated: 2023/09/20 14:05:24 by idumenil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,33 +21,39 @@
 # include <X11/keysym.h>
 # include "minilibx-linux/mlx.h"
 
-#define ERROR_MESSAGE "Please enter \n\t\"./fractol mandelbrot\" or \n\t\"./fractol julia <value_1> <value_2>\"\n"
+# define ERROR_MESSAGE "Wrong format\n"
 
-#define WIDTH	800
-#define	HEIGHT	800
+# define WIDTH	800
+# define HEIGHT	800
 
-#define BLACK       0x000000
-#define WHITE       0xFFFFFF
-#define RED         0xFF0000
-#define GREEN       0x00FF00
-#define BLUE        0x0000FF
+# define BLACK       0x000000
+# define WHITE       0xFFFFFF
+# define RED         0xFF0000
+# define GREEN       0x00FF00
+# define BLUE        0x0000FF
 
-#define MAGENTA_BURST   0xFF00FF
-#define LIME_SHOCK      0xCCFF00
-#define NEON_ORANGE     0xFF6600
-#define PSYCHEDELIC_PURPLE 0x660066
-#define AQUA_DREAM      0x33CCCC
-#define HOT_PINK        0xFF66B2
-#define ELECTRIC_BLUE   0x0066FF
-#define LAVA_RED        0xFF3300
+# define MAGENTA_BURST   0xFF00FF
+# define LIME_SHOCK      0xCCFF00
+# define NEON_ORANGE     0xFF6600
+# define PSYCHEDELIC_PURPLE 0x660066
+# define AQUA_DREAM      0x33CCCC
+# define HOT_PINK        0xFF66B2
+# define ELECTRIC_BLUE   0x0066FF
+# define LAVA_RED        0xFF3300
 
-typedef struct	s_complex
+typedef struct s_complex
 {
 	double	x;
 	double	y;
 }				t_complex;
 
-typedef struct	s_img
+typedef struct s_range
+{
+	double	min;
+	double	max;
+}				t_range;
+
+typedef struct s_img
 {
 	void	*img_ptr;
 	char	*pixels_ptr;
@@ -56,14 +62,12 @@ typedef struct	s_img
 	int		line_len;
 }				t_img;
 
-typedef struct	s_fractal
+typedef struct s_fractal
 {
 	char	*name;
 	void	*mlx_connection;
 	void	*mlx_window;
 	t_img	img;
-
-	//Hooks member variables
 	double	escape_value;
 	int		iterations_defintion;
 	double	shift_x;
@@ -73,16 +77,17 @@ typedef struct	s_fractal
 	double	julia_y;
 }				t_fractal;
 
-
-
-int		ft_strncmp(char *s1, char *s2, int n);
-void    putstr_fd(char *s, int fd);
-double  atodbl(char *s);
-void    fractal_init(t_fractal *fractal);
-void    fractal_render(t_fractal *fractal);
+int			ft_strncmp(char *s1, char *s2, int n);
+void		putstr_fd(char *s, int fd);
+double		atodbl(char *s);
+void		fractal_init(t_fractal *fractal);
+void		fractal_render(t_fractal *fractal);
 double		map(double unscaled_num, double new_min, double new_max, double old_min, double old_max);
-t_complex   sum_complex(t_complex z1, t_complex z2);
-t_complex   square_complex(t_complex z);
+t_complex	sum_complex(t_complex z1, t_complex z2);
+t_complex	square_complex(t_complex z);
 int			key_handler(int keysym, t_fractal *fractal);
-int    	close_handler(t_fractal *fractal);
-int		mouse_handler(int button, int x, int y, t_fractal *fractal);
+int			close_handler(t_fractal *fractal);
+int			mouse_handler(int button, int x, int y, t_fractal *fractal);
+int			julia_track(int x, int y, t_fractal *fractal);
+
+#endif
