b34ce8175bc765745206b66a151f551bce5f21b0
Irène Duménil <126900447+idumenil@users.noreply.github.com>
2023-09-20 14:50:35 +0200

    Update math_utils.c

--- math_utils.c
+++ math_utils.c
@@ -1,25 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   math_utils.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: idumenil <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/18 10:55:23 by idumenil          #+#    #+#             */
+/*   Updated: 2023/09/19 12:18:05 by idumenil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "fractol.h"
-
-
-double map(double unscaled_num, double new_min, double new_max, double old_min, double old_max)
+/*
+double	map(double unscaled_num, double new_min, double new_max, \
+	double old_min, double old_max)
 {
-    return (new_max - new_min) * (unscaled_num - old_min) / (old_max - old_min) + new_min;
+	return ((new_max - new_min) * (unscaled_num - old_min) / \
+	(old_max - old_min) + new_min);
+}
+*/
+
+double	map(double unscaled_num, t_range new_range, t_range old_range)
+{
+	return ((new_range.max - new_range.min) * (unscaled_num - old_range.min) / \
+	(old_range.max - old_range.min) + new_range.min);
 }
 
-t_complex   sum_complex(t_complex z1, t_complex z2)
+t_complex	sum_complex(t_complex z1, t_complex z2)
 {
-    t_complex   result;
+	t_complex	result;
 
-    result.x = z1.x + z2.x;
-    result.y = z1.y + z2.y;
-    return result;
+	result.x = z1.x + z2.x;
+	result.y = z1.y + z2.y;
+	return (result);
 }
 
-t_complex   square_complex(t_complex z)
+t_complex	square_complex(t_complex z)
 {
-    t_complex   result;
-    
-    result.x = (z.x * z.x) - (z.y * z.y);
-    result.y = 2 * z.x * z.y;
-    return result;
+	t_complex	result;
+
+	result.x = (z.x * z.x) - (z.y * z.y);
+	result.y = 2 * z.x * z.y;
+	return (result);
 }
