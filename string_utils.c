f5798295ccbe0c7150f7e17aa636f0ed082ed74b
Irène Duménil <126900447+idumenil@users.noreply.github.com>
2023-09-23 13:00:05 +0200

    Update string_utils.c

--- string_utils.c
+++ string_utils.c
@@ -6,29 +6,28 @@
 /*   By: idumenil <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/07 15:53:08 by idumenil          #+#    #+#             */
-/*   Updated: 2023/09/07 15:53:10 by idumenil         ###   ########.fr       */
+/*   Updated: 2023/09/21 12:19:29 by idumenil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
-
 int	ft_strncmp(char *s1, char *s2, int n)
 {
-	if (NULL == s1 || NULL == s2 || n <= 0)
+	if (!s1 || !s2 || n <= 0)
 		return (0);
 	while (*s1 == *s2 && n > 0 && *s1 != '\0')
 	{
-		++s1;
-		++s2;
-		--n;
+		s1++;
+		s2++;
+		n--;
 	}
 	return (*s1 - *s2);
 }
 
 void	putstr_fd(char *s, int fd)
 {
-	if (NULL == s || fd < 0)
+	if (!s || fd < 0)
 		return ;
 	if (*s != '\0')
 	{
@@ -41,26 +40,26 @@ double	atodbl(char *s)
 {
 	long	integer_part;
 	double	fractional_part;
-	double	pow;
+	double	power;
 	int		sign;
 
 	integer_part = 0;
 	fractional_part = 0;
-	sign = +1;
-	pow = 1;
-	while ((*s >= 9 && *s <= 13) || 32 == *s)
-		++s;
+	sign = 1;
+	power = 1;
+	while ((*s >= 9 && *s <= 13) || *s == 32)
+		s++;
 	while ('+' == *s || '-' == *s)
 		if ('-' == *s++)
 			sign = -sign;
 	while (*s != '.' && *s)
 		integer_part = (integer_part * 10) + (*s++ - 48);
 	if ('.' == *s)
-		++s;
+		s++;
 	while (*s)
 	{
-		pow /= 10;
-		fractional_part = fractional_part + (*s++ - 48) * pow;
+		power /= 10;
+		fractional_part = fractional_part + (*s++ - 48) * power;
 	}
 	return ((integer_part + fractional_part) * sign);
 }
